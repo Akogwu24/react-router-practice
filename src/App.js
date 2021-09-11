09eef6609f0567acf291b6e79261b813c7f1da80
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-09-11 15:50:36 +0100

    redirect routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import { NotFound } from './pages/404';
 import About from './pages/About';
@@ -8,14 +9,20 @@ import Post from './pages/Post';
 import Profile from './pages/Profile';
 
 function App() {
+  const [login, setLogin] = useState();
   return (
-    <BrowserRouter basename='/basename_akogwu'>
+    <BrowserRouter>
       <div className='App'>
         <Header />
+        <button onClick={() => setLogin(!login)}>
+          {login ? 'logout' : 'login'}
+        </button>
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' component={About} />
-          <Route path='/profile' component={Profile} />
+          <Route path='/profile'>
+            {login ? <Profile /> : <Redirect to='/' />}
+          </Route>
           <Route path='/post/:id' component={Post} />
           <Route component={NotFound} />
         </Switch>
